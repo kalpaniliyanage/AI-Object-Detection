f867a073a9bf694e046da095826d650478b4b19d
Kalpani M Liyanage <kalpanimadhubashini29@gmail.com>
2026-07-06 03:01:25 +0530

    Load YOLO-World model and define classes
    Added YOLO-World model loading and class setup.

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
 import clip
 model, preprocess = clip.load("ViT-B/32", jit=False)
 print("CLIP loaded fine")
+
+from ultralytics import YOLOWorld
+
+model = YOLOWorld("yolov8s-world.pt")
+
+print("YOLO-World model loaded successfully!")
+
+classes = [
+    "person",
+    "book",
+    "bottle",
+    "cup",
+    "cell phone",
+]
+
+model.set_classes(classes)
+
+print("Classes loaded:")
+for item in classes:
+    print("-", item)
